76416db9d800d7d28993b089a8d5e8e5a31baa82
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-03-06 16:23:24 +0700

    refactor: add package directory management

--- app/src/main/java/com/fiknaufalh/coffeenutrition/activities/MainActivity.kt
+++ app/src/main/java/com/fiknaufalh/coffeenutrition/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fiknaufalh.coffeenutrition
+package com.fiknaufalh.coffeenutrition.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fiknaufalh.coffeenutrition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
