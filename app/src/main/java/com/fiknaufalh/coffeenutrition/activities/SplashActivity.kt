69a4e6d601bbed52bb018b9ed36b1aa36c616c1e
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-03-07 01:05:31 +0700

    feat: add splash screen and change icon

---
+++ app/src/main/java/com/fiknaufalh/coffeenutrition/activities/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.fiknaufalh.coffeenutrition.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+import com.fiknaufalh.coffeenutrition.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
